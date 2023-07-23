d785f0e7c8cc71a416e7d5e9fa4f92518ec0e03c
PoppyTillott <popstill20@gmail.com>
2023-07-23 15:29:53 +0100

    added dictionary attack. hope this works haha

--- frame.py
+++ frame.py
@@ -1,22 +1,51 @@
 #importing our pop up window from tkinter
 import tkinter
 from tkinter import messagebox #pop up box appears outside our window
-#haslib import needed...
+#import packages for dictionary attack - for hashing and reading urls
+import hashlib
+import urllib.request
 
+#Function to read url text file of the 10,000 most common passwords which will be used a dictionary
+#Define function and variable being parsed (the url address)
+def read_dictionary(url):
+     #Declare and make variable global to be used throughout the program
+     global common_passwords
+     #Variable 'common_passwords' turned into an empty list
+     common_passwords = []
+     #Open url text file and run through each line
+     for line in urllib.request.urlopen(url):
+          #Decode each line of text using Unicode so each line becomes a separate string
+          url_line = line.decode('utf-8')
+          #Use strip to remove \n in each line
+          #Add the each word to the common_passwords list
+          common_passwords.append(url_line.strip())
+    
+#Function for dictionary attack (hash password and list then compare values)
+def compare_passwords():
+    #assign user's entered password from window input to a variable
+    password = password_entry.get()
+    #hash user password
+    hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    #For loop passing through each entry in the common_passwords list
+    for word in common_passwords:
+        #hash each value in the list as it passes through the loop
+        hashed_word = hashlib.sha256(word.encode('utf-8')).hexdigest()
+        #compare each hashed list value with the hashed user password
+        if hashed_password == hashed_word:
+            #If a match is find, send a message box notifying the user 
+            messagebox.showinfo(title="Password was cracked", message=f"'{password}' is a bad password.")
+            #Break the for loop as there's no need to iterate through again
+            break
+    #If all items in the dictionary list have been iterated through and no match has been found,
+    #Follow this path
+    else:
+        #Notify user the dictionary attack was unsuccessful
+        messagebox.showerror(title="Crack unsuccessful", message="Finding another way..")#pop up box which shows invalid login needs to change to password cracker 
 
-
-
-#DICTIONARY ATTACK CODE NEEDED FOR CODE TO WORK
-#FOLLOWING CODE IS FOR THE GUI 
-
-
-
-
-
-
-
-
-
+#The url leading the to the list of 10,000 words for the dictionary attack
+url ='https://lucidar.me/en/security/files/10000-most-common-passwords.txt'
+#Call the function to load the dictionary
+read_dictionary(url)
 
 
 
